14250cb086d578beb4c25656d8be6738bc4cef17
Ritu Rani <98016078+Riturani87@users.noreply.github.com>
2022-12-18 23:23:18 +0530

    Update Sample.cpp

--- Control_statement/Sample.cpp
+++ Control_statement/Sample.cpp
@@ -1,1 +1,29 @@
-#include<iostream.h>
+#include <iostream>
+using namespace std;
+
+
+int main() {
+  int a,b;
+  cout << "enter the value of a \n";
+  cin>>a;
+  cout <<"enter the value of b\n";
+  cin>>b;
+  char op;
+  cout<<" enter the operation you want to perform\n";
+  cin>>op;
+switch(op){
+  case '+':cout<<(a+b);
+  break;
+    case '-':cout<<(a-b);
+  break;
+  case '*':cout<<(a*b);
+  break;
+    case '/':cout<<(a/b);
+  break;
+    case'%':cout<<(a%b);
+    break;
+ default:cout<<"please enter the valid operation \n";
+  break;
+}  
+  return 0;
+}
